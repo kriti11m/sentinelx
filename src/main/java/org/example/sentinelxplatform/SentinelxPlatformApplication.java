4e0c316137fa159a72aeb6cd133060d6768fd32c
kriti11m <kriti.maheshwari2023@vitstudent.ac.in>
2026-05-14 16:19:50 +0530

    Initial project setup

---
+++ src/main/java/org/example/sentinelxplatform/SentinelxPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.example.sentinelxplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelxPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelxPlatformApplication.class, args);
+    }
+
+}
